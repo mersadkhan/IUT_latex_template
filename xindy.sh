20143e46da7a80be7751f91e5023f3843c0c9468
Mersad Mostaghimi <mersad.mostaghimi@kit.edu>
2024-03-05 13:10:34 +0100

    the first update on git after changing the fint to vazirmatn
    Signed-off-by: Mersad Mostaghimi <mersad.mostaghimi@kit.edu>

---
+++ xindy.sh
@@ -0,0 +1,3 @@
+xindy -L persian-variant3 -C utf8 -I xindy -M main.xdy -t main.glg -o main.gls main.glo  
+xindy -L persian-variant3 -C utf8 -I xindy -M main.xdy -t main.blg -o main.bls main.blo 
+xindy -L english -C utf8 -I xindy -M main.xdy -t main.alg -o main.acr main.acn
